b884eeb8dc39a170cab2410e3c3cc0619a60bc41
khatrikajal <123454266+khatrikajal@users.noreply.github.com>
2025-09-27 10:54:04 +0530

    remove otp code

--- Backend/apps/authentication/urls.py
+++ Backend/apps/authentication/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from .views import SendOtpAPIView, VerifyOtpAPIView
 
 urlpatterns = [
-    path("send-otp/", SendOtpAPIView.as_view(), name="send-otp"),
-    path("verify-otp/", VerifyOtpAPIView.as_view(), name="verify-otp"),
 ]
